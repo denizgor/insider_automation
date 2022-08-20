4be003ca55f9265d03f67132af127a3ddf571df9
denizgor <71087556+denizgor@users.noreply.github.com>
2022-08-20 16:23:06 +0300

    Update bolunen_sayi_bulma.py
    Kullanıcıdan input alma fonksiyonu eklendi
    
    Input kontrolleri eklendi

--- bolunen_sayi_bulma.py
+++ bolunen_sayi_bulma.py
@@ -1,8 +1,27 @@
 def bolunen_sayi_bulma(min_sayi, max_sayi, bolen_sayi):
   tam_bolunenler = []
+
+  min_sayi = int(input("Minimum sayıyı girin:\n"))
+  while min_sayi <= 0:
+    print("Minimum sayı 0'dan büyük olmalıdır")
+    min_sayi = int(input("Minimum sayıyı girin:\n"))
+
+  max_sayi = int(input("Maksimum sayıyı girin:\n"))
+  while max_sayi < min_sayi:
+    print("Maksimum sayı minimum sayıdan küçük olamaz.")
+    max_sayi = int(input("Maksimum sayıyı girin:\n"))
+  
+  bolen_sayi = int(input("Bölen sayıyı girin:\n"))
+  while bolen_sayi > max_sayi:
+    print("Bölen sayı maksimum sayıdan büyük olamaz.")
+    bolen_sayi = int(input("Bölen sayıyı girin:\n"))
+  
   for sayi in range(min_sayi, max_sayi):
     if sayi % bolen_sayi == 0:
-      tam_bolunenler.append(sayi) 
+      tam_bolunenler.append(sayi)
+  
+  print("Tam Bölünenler: ",tam_bolunenler)
+  print("Tam Bölünen Sayı Adedi: ", len(tam_bolunenler))
   return(tam_bolunenler, len(tam_bolunenler))
   
-bolunen_sayi_bulma(12,103,6)
+bolunen_sayi_bulma(min_sayi="", max_sayi="", bolen_sayi="")
