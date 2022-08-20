d0df63a0bb82dce1f66be9be3725c3c1b1c6e6cc
denizgor <71087556+denizgor@users.noreply.github.com>
2022-08-20 14:31:22 +0300

    Update sayi_okunusu.py

--- sayi_okunusu.py
+++ sayi_okunusu.py
@@ -1,63 +1,32 @@
-
-
 def sayi_atama():
   sayi_input = input("İki basamaklı bir sayi girin. ")
   return sayi_input
-
-
 
 def sayi_okunusu():
   atanan_sayi = int(sayi_atama())
   basamaklar = []
   
-  if atanan_sayi >= 10 and atanan_sayi <= 99:
-    for sayi in str(atanan_sayi):
+  for sayi in str(atanan_sayi):
       basamaklar.append(int(sayi))
-    
-    if basamaklar[0] == 1:
-      birinci_basamak = "on"
-    elif basamaklar[0] == 2:
-      birinci_basamak = "yirmi"
-    elif basamaklar[0] == 3:
-      birinci_basamak = "otuz"
-    elif basamaklar[0] == 4:
-      birinci_basamak = "kırk"
-    elif basamaklar[0] == 5:
-      birinci_basamak = "elli"
-    elif basamaklar[0] == 6:
-      birinci_basamak = "altmış"
-    elif basamaklar[0] == 7:
-      birinci_basamak = "yetmiş"
-    elif basamaklar[0] == 8:
-      birinci_basamak = "seksen"
-    elif basamaklar[0] == 9:
-      birinci_basamak = "doksan"
-   
+
+  onluk = ""
+  birlik = ""
   
-    if basamaklar[1] == 1:
-      ikinci_basamak = "bir"
-    elif basamaklar[1] == 2:
-      ikinci_basamak = "iki"
-    elif basamaklar[1] == 3:
-      ikinci_basamak = "üç"
-    elif basamaklar[1] == 4:
-      ikinci_basamak = "dört"
-    elif basamaklar[1] == 5:
-      ikinci_basamak = "beş"
-    elif basamaklar[1] == 6:
-      ikinci_basamak = "altı"
-    elif basamaklar[1] == 7:
-      ikinci_basamak = "yedi"
-    elif basamaklar[1] == 8:
-      ikinci_basamak = "sekiz"
-    elif basamaklar[1] == 9:
-      ikinci_basamak = "dokuz"
-    elif basamaklar[1] == 0:
-      ikinci_basamak = ""
+  okunuslar = {1 : ["on", "bir"], 2 : ["yirmi", "iki"], 3 : ["otuz", "üç"], 4 : ["kırk",   "dört"], 5 : ["elli", "beş"], 
+               6 : ["altmış", "altı"], 7 : ["yetmiş", "yedi"], 8 : ["seksen", "sekiz"], 9 : ["doksan","dokuz"], }
+  
+  if atanan_sayi >= 10 and atanan_sayi <= 99:
+      for key in okunuslar:
+        if basamaklar[0] == key:
+            onluk = (okunuslar[key][0])
+        if basamaklar[1] == key:
+            birlik = (okunuslar[key][1])      
    
-    print(birinci_basamak + ikinci_basamak)
+      print(onluk + birlik)
   else:
     print("Sadece iki basamaklı sayılar girebilirsiniz. \n")
     sayi_okunusu()
+
+sayi_okunusu()  
 
 sayi_okunusu()  
