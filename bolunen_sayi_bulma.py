3f1133cd43556cfa94eb995e48dec2f915b2bf42
denizgor <71087556+denizgor@users.noreply.github.com>
2022-08-20 12:14:02 +0300

    Update bolunen_sayi_bulma.py

--- bolunen_sayi_bulma.py
+++ bolunen_sayi_bulma.py
@@ -2,10 +2,7 @@ def bolunen_sayi_bulma(min_sayi, max_sayi, bolen_sayi):
   tam_bolunenler = []
   for sayi in range(min_sayi, max_sayi):
     if sayi % bolen_sayi == 0:
-      tam_bolunenler.append(sayi)
-  # print(range(min_sayi, max_sayi))
-  # print("Tam Bölünenler: ",tam_bolunenler)
-  # print("Değerler: ", len(tam_bolunenler))
+      tam_bolunenler.append(sayi) 
   return(tam_bolunenler, len(tam_bolunenler))
   
 bolunen_sayi_bulma(12,103,6)
