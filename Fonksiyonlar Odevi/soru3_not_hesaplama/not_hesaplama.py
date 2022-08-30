a65a295f6def2611ccde760fe24b90908b760fba
denizgor <71087556+denizgor@users.noreply.github.com>
2022-08-31 00:04:43 +0300

    Update not_hesaplama.py

--- Fonksiyonlar Odevi/soru3_not_hesaplama/not_hesaplama.py
+++ Fonksiyonlar Odevi/soru3_not_hesaplama/not_hesaplama.py
@@ -16,6 +16,9 @@ def nothesaplama():
     
   toplamnot = round((vize1 * 0.30) + (vize2 * 0.30) + (final * 0.40))
   
+  # Notları dictionary ile tutmak mümkün değil çünkü tek bir key-value çifti yerine bir aralık vermek gerekiyor. 
+  # Aksi halde daha fazla kod yazmak gerek.
+  
   if toplamnot >= 90:
     print("Öğrenci Notu: AA")
   elif toplamnot >= 85:
@@ -36,84 +39,3 @@ def nothesaplama():
     print("Öğrenci Notu: FF")
 
 nothesaplama()
-
-
-
-
-
-
-###OLD
-# def nothesaplama():
- 
-#   def kullanici_girdi_kontrol(kullanici_girdisi):
-#     girdi_kontrol = False
-#     while girdi_kontrol == False:
-#       if  kullanici_girdisi >= 0 and kullanici_girdisi <= 100:
-#         girilen_not = kullanici_girdisi
-#         girdi_kontrol = True
-#         return girilen_not
-#       else:
-#         print("Yalnızca 0 ile 100 arasında bir sayı girin. \n")
-  
- 
- 
-#   vize1 = kullanici_girdi_kontrol(int(input("Birinci vize notunu girin:\n (0 - 100) ")))
-#   vize2 = kullanici_girdi_kontrol(int(input("İkinci vize notunu girin:\n (0 - 100) ")))
-#   final = kullanici_girdi_kontrol(int(input("Final notunu girin:\n (0 - 100) ")))
-
-#   toplamnot = round((vize1 * 0.30) + (vize2 * 0.30) + (final * 0.40))
-
-# #Dictionary ile yapılıyor mu dene!!!
-  
-#   if toplamnot >= 90:
-#     print("Öğrenci Notu: AA")
-#   elif toplamnot >= 85:
-#     print("Öğrenci Notu: BA")
-#   elif toplamnot >= 80:
-#     print("Öğrenci Notu: BB")
-#   elif toplamnot >= 75:
-#     print("Öğrenci Notu: CB")
-#   elif toplamnot >= 70:
-#     print("Öğrenci Notu: CC")
-#   elif toplamnot >= 65:
-#     print("Öğrenci Notu: DC")
-#   elif toplamnot >= 60:
-#     print("Öğrenci Notu: DD")
-#   elif toplamnot >= 55:
-#     print("Öğrenci Notu: FD")
-#   elif toplamnot < 50:
-#     print("Öğrenci Notu: FF")
-
-
-
-# nothesaplama()
-
-
-
-
-#################
-# notlar = []
-
-#   def kullanici_girdi_kontrol(kullanici_girdisi):
-#     sinavlar = [vize1(), vize2(), final()]
-#     for ogrenci_notu in sinavlar:
-#       if sinavlar[ogrenci_notu] >= 0 and sinavlar[ogrenci_notu] <= 100:
-#         return kullanici_girdisi
-#       else:
-#         print("Yalnızca 0 ile 100 arasında bir sayı girin. \n")
-#         sinavlar[ogrenci_notu]
-        
-#   def vize1():
-#     kullanici_girdi_kontrol(int(input("Birinci vize notunu girin:\n (0 - 100) ")))
-    
-#   def vize2():
-#     kullanici_girdi_kontrol(int(input("İkinci vize notunu girin:\n (0 - 100) ")))
-    
-#   def final():
-#     kullanici_girdi_kontrol(int(input("Final notunu girin:\n (0 - 100) ")))
-    
-  
-#   vize1()
-#   vize2()
-#   final()
-  
