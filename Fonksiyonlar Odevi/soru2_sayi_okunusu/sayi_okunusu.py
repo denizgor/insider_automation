97837408c6503ef5d26fd7cf2b838de3e7b10b99
denizgor <71087556+denizgor@users.noreply.github.com>
2022-09-11 19:30:55 +0300

    Update sayi_okunusu.py
    - 0 için bir kondüsyon eklendi
    - For döngüsü içindeki üçüncü "if" "elif" oldu

--- Fonksiyonlar Odevi/soru2_sayi_okunusu/sayi_okunusu.py
+++ Fonksiyonlar Odevi/soru2_sayi_okunusu/sayi_okunusu.py
@@ -1,32 +1,37 @@
 def sayi_atama():
-  sayi_input = input("İki basamaklı bir sayi girin. ")
-  
-  return sayi_input
+    sayi_input = input("İki basamaklı bir sayi girin. ")
+
+    return sayi_input
+
 
 def sayi_okunusu():
-  atanan_sayi = int(sayi_atama())
- 
-  birlik = atanan_sayi % 10
-  onluk = (atanan_sayi - birlik) / 10
-  
-  okunuslar = {1 : ["on", "bir"], 2 : ["yirmi", "iki"], 3 : ["otuz", "üç"], 4 : ["kırk",   "dört"], 5 : ["elli", "beş"], 
-               6 : ["altmış", "altı"], 7 : ["yetmiş", "yedi"], 8 : ["seksen", "sekiz"], 9 : ["doksan","dokuz"], }
-  
-  if atanan_sayi >= 10 and atanan_sayi <= 99:
-      for key in okunuslar:
-        if basamaklar[0] == key:
-            onluk = (okunuslar[key][0])
-            
-        if basamaklar[1] == key:
-            birlik = (okunuslar[key][1])
-            
-        if type(onluk) is str and type(birlik) is str:
-          break
-          
-      print(f" Sayı okunuşu: {onluk}{birlik}")
-  else:
-    print("Sadece iki basamaklı sayılar girebilirsiniz. \n")
-    
-    sayi_okunusu()
+    atanan_sayi = int(sayi_atama())
+
+    if atanan_sayi >= 10 and atanan_sayi <= 99:
+        
+        birlik = atanan_sayi % 10
+        onluk = int((atanan_sayi - birlik) / 10)
+
+        okunuslar = {0: ["", ""], 1: ["on", "bir"], 2: ["yirmi", "iki"], 3: ["otuz", "üç"], 4: ["kırk", "dört"],
+                 5: ["elli", "beş"], 6: ["altmış", "altı"], 7: ["yetmiş", "yedi"], 8: ["seksen", "sekiz"],
+                 9: ["doksan", "dokuz"], }
+
+        for key in okunuslar:
+            if onluk == key:
+                onluk = (okunuslar[key][0])
+
+            if birlik == key:
+                birlik = (okunuslar[key][1])
+
+            elif type(onluk) is str and type(birlik) is str:
+                break
+
+    else:
+        print("Sadece iki basamaklı sayılar girebilirsiniz. \n")
+
+        sayi_okunusu()
+
+    okunus = "Sayı okunuşu: {}{}"
+    print(okunus.format(onluk, birlik))
 
 sayi_okunusu()
