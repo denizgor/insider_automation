83d285e103b98c89cfeb6d3734f0de1e735d746d
denizgor <71087556+denizgor@users.noreply.github.com>
2022-09-11 19:34:51 +0300

    Update not_hesaplama.py
    - toplamnot > toplam_not olarak değiştirildi
    - return ifadelerinden sonra boşluklar eklendi
    - if dizisinin sonuna "else" eklendi

--- Fonksiyonlar Odevi/soru3_not_hesaplama/not_hesaplama.py
+++ Fonksiyonlar Odevi/soru3_not_hesaplama/not_hesaplama.py
@@ -2,6 +2,7 @@ def kullanici_girdi_kontrol(kullanici_girdisi):
   if kullanici_girdisi >= 0 and kullanici_girdisi <= 100:
     
     return kullanici_girdisi
+  
   else:
     kullanici_girdisi = int(input("Yalnızca 0 ile 100 arasında bir sayı girin. \n"))
     
@@ -16,23 +17,23 @@ vize2 = kullanici_girdi_kontrol(vize2)
 final = int(input("Final notunu girin:\n (0 - 100) "))
 final = kullanici_girdi_kontrol(final)
 
-toplamnot = round((vize1 * 0.30) + (vize2 * 0.30) + (final * 0.40))
+toplam_not = round((vize1 * 0.30) + (vize2 * 0.30) + (final * 0.40))
 
-if toplamnot >= 90:
+if toplam_not >= 90:
   print("Öğrenci Notu: AA")
-elif toplamnot >= 85:
+elif toplam_not >= 85:
   print("Öğrenci Notu: BA")
-elif toplamnot >= 80:
+elif toplam_not >= 80:
   print("Öğrenci Notu: BB")
-elif toplamnot >= 75:
+elif toplam_not >= 75:
   print("Öğrenci Notu: CB")
-elif toplamnot >= 70:
+elif toplam_not >= 70:
   print("Öğrenci Notu: CC")
-elif toplamnot >= 65:
+elif toplam_not >= 65:
   print("Öğrenci Notu: DC")
-elif toplamnot >= 60:
+elif toplam_not >= 60:
   print("Öğrenci Notu: DD")
-elif toplamnot >= 55:
+elif toplam_not >= 55:
   print("Öğrenci Notu: FD")
-elif toplamnot < 50:
+else toplam_not < 50:
   print("Öğrenci Notu: FF")
