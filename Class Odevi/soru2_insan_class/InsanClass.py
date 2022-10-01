a6bf1128cc1a969b68b4feac496b5bda13a70cc3
denizgor <71087556+denizgor@users.noreply.github.com>
2022-10-01 15:41:29 +0300

    - Readme dosyası eklendi - Test kodları README.md dosyasına taşındı

--- Class Odevi/soru2_insan_class/InsanClass.py
+++ Class Odevi/soru2_insan_class/InsanClass.py
@@ -1,34 +1,16 @@
-class Insan():
-    def __init__(self, ad = "Ahmet", soyad = "Bulut", yas = 21, ulke = "Türkiye", sehir = "Ankara", yetenekler = []):
+class Insan:
+    def __init__(self, ad="Ahmet", soyad="Bulut", yas=21, ulke="Türkiye", sehir="Ankara"):
         self.ad = ad
         self.soyad = soyad
         self.yas = yas
         self.ulke = ulke
         self.sehir = sehir
-        self.yetenekler = yetenekler
+        self.yetenekler = []
 
     def kisi_bilgileri(self):
         bilgiler = "Ad: {}\nSoyad: {}\nYaş: {}\nÜlke: {}\nŞehir: {}\nYetenekler: {}\n"
-        return(bilgiler.format(Insan().ad, Insan().soyad, Insan().yas, Insan().ulke, Insan().sehir, Insan().yetenekler))
-
+        return bilgiler.format(self.ad, self.soyad, self.yas, self.ulke, self.sehir, self.yetenekler)
 
     def yetenek_ekle(self):
         yeni_yetenek = input("Eklemek istediğiniz yeteneği yazın:\n")
-        Insan().yetenekler.append(yeni_yetenek)
-        #print(Insan().kisi_bilgileri())
-        print("Yetenekler:", Insan().yetenekler)
-
-
-#Test
-insan1 = Insan().kisi_bilgileri()
-print(insan1)
-
-#tek bir parametre alma
-#print(Insan().kisi_bilgileri(Insan().ad))
-#çalışmadı
-
-insan2 = Insan("Mustafa", "Akıncı", 24, "Türkiye", "İzmir").kisi_bilgileri()
-Insan().yetenek_ekle()
-print("İnsan 2:", insan2)
-
-
+        self.yetenekler.append(yeni_yetenek)
