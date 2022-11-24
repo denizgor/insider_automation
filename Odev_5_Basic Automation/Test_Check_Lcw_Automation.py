ca93d4f302ad033ac87cfab1890a530162d823fb
denizgor <71087556+denizgor@users.noreply.github.com>
2022-11-24 16:08:28 +0300

    -Dosya adındaki 'LCW' yazısı değiştirildi -Class adındaki 'LCW' yazısı değiştirildi -Test adındaki 'LCW' yazısı değiştirildi -assertIn kullanıldı

--- Odev_5_Basic Automation/Test_Check_Lcw_Automation.py
+++ Odev_5_Basic Automation/Test_Check_Lcw_Automation.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ActionChains
 
 
-class TestCheckLCWAutomation(unittest.TestCase):
+class TestCheckLcwAutomation(unittest.TestCase):
     EV_YASAM_CAT = (By.LINK_TEXT, "EV & YAŞAM")
     CATAL_KASIK_CAT = (By.LINK_TEXT, "Çatal, Kaşık ve Bıçak")
     CATAL_KASIK_BRD = (By.CLASS_NAME, "lcw-breadcrumb__item-list__item:last-child")
@@ -18,20 +18,24 @@ class TestCheckLCWAutomation(unittest.TestCase):
     DELETE_ITEM_ICON = (By.CSS_SELECTOR, "[title='Sil']")
     DELETE_BTN = (By.LINK_TEXT, "Sil")
     EMPTY_CART_TEXT = (By.CLASS_NAME, "cart-empty-title")
+    CART_HEADER = (By.CSS_SELECTOR, "div.cart-header")
     LCW_LOGO = (By.CSS_SELECTOR, "a.main-header-logo")
+
 
     base_url = "https://www.lcwaikiki.com/tr-TR/TR"
     catal_kasik_brd = "Çatal, Kaşık, Bıçak Setleri"
     proceed_to_pay_txt = "ÖDEME ADIMINA GEÇ"
+    wait_duration = 10
 
     def setUp(self):
         self.driver = webdriver.Chrome(ChromeDriverManager().install())
         self.driver.maximize_window()
         self.driver.get(self.base_url)
-        self.driver.implicitly_wait(10)
-        self.wait = WebDriverWait(self.driver, 10)
+        self.driver.implicitly_wait(self.wait_duration)
 
-    def test_check_LCW_automation(self):
+        self.wait = WebDriverWait(self.driver, self.wait_duration)
+
+    def test_check_lcw_automation(self):
         actions = ActionChains(self.driver)
         actions.move_to_element(self.driver.find_element(*self.EV_YASAM_CAT)).perform()
         actions.move_to_element(self.driver.find_element(*self.CATAL_KASIK_CAT)).click().perform()
@@ -48,12 +52,12 @@ class TestCheckLCWAutomation(unittest.TestCase):
                         "Sepete ürün eklenmedi.")
 
         self.driver.find_element(*self.CART_COUNT_BADGE).click()
-        self.assertEqual(self.proceed_to_pay_txt, self.driver.find_element(*self.PROCEED_TO_PAY_TXT).text,
-                         "Couldn't find 'ÖDEME ADIMINA GEÇ'.")
+        self.assertIn("Sepetim", self.driver.find_element(*self.CART_HEADER).text,
+                      "Couldn't find 'Sepetim' in cart header.")
 
         self.driver.find_element(*self.DELETE_ITEM_ICON).click()
         self.assertTrue(EC.presence_of_element_located(self.driver.find_element(*self.DELETE_BTN)),
-                        "Item Delete Modal not visible.")
+                        "'Item Delete Modal' not visible.")
 
         self.driver.find_element(*self.DELETE_BTN).click()
         self.assertTrue(EC.presence_of_element_located(self.driver.find_element(*self.EMPTY_CART_TEXT)),
@@ -63,5 +67,5 @@ class TestCheckLCWAutomation(unittest.TestCase):
         self.assertEqual(self.base_url, self.driver.current_url,
                          "Is not on mainpage.")
 
-    def tearDown(self) -> None:
+    def tearDown(self):
         self.driver.quit()
